82829c74389940e1bec2f48d89fdf09f1d67dfc9
MLStan <veldhuizenstan@gmail.com>
2021-04-25 19:24:05 +0200

    clear messages delete

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const prefix = 'w?';
 
 const fs = require('fs');
 const membercount = require('./commands/member-count')
-const CLEAR_MESSAGES = 'clearMessages';
 
 client.commands = new Discord.Collection();
 
@@ -71,34 +70,6 @@ client.on('message', message => {
 
     if(command === 'ticket'){
         client.commands.get('ticket').execute(message, args, Discord);
-    }
-
-    if (message.content == CLEAR_MESSAGES) {
-  
-        if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
-          message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-          console.log("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-          return;
-        } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
-          message.channel.sendMessage("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-          console.log("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-          return;
-        }
-  
-        if (message.channel.type == 'text') {
-          message.channel.fetchMessages()
-            .then(messages => {
-              message.channel.bulkDelete(messages);
-              messagesDeleted = messages.array().length; 
-  
-              message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
-              console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
-            })
-            .catch(err => {
-              console.log('Error while doing Bulk Delete');
-              console.log(err);
-            });
-        }
     }
 
     
