b63545c116a48deffe4c6be3cf087763b65618f1
MLStan <veldhuizenstan@gmail.com>
2021-04-25 17:18:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,35 @@ for(const file of commandFiles){
 client.once('ready', () => {
     console.log('Wascoi is klaar voor gebruik!')
 
-    membercount(client)
+    membercount(client)  
+
+    command(client, 'kick', (message) => {
+        const { member, mentions } = message
+    
+        const tag = `<@${member.id}>`
+    
+        if (
+          member.hasPermission('ADMINISTRATOR') ||
+          member.hasPermission('KICK_MEMBERS')
+        ) {
+          const target = mentions.users.first()
+          if (target) {
+            const targetMember = message.guild.members.cache.get(target.id)
+            targetMember.kick()
+            message.channel.send(`${tag} That user has kicked`)
+          } else {
+            message.channel.send(`${tag} Please specify someone to kick.`)
+          }
+        } else {
+          message.channel.send(
+            `${tag} You do not have permission to use this command.`
+          )
+        }
+    })
 });
+
+
+
 
 
 
