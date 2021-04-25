f07176832a99a3938b6c431c3d8ed3a61141eccb
MLStan <veldhuizenstan@gmail.com>
2021-04-25 17:21:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,29 +24,6 @@ client.once('ready', () => {
 
     membercount(client)  
 
-    command(client, 'kick', (message) => {
-        const { member, mentions } = message
-    
-        const tag = `<@${member.id}>`
-    
-        if (
-          member.hasPermission('ADMINISTRATOR') ||
-          member.hasPermission('KICK_MEMBERS')
-        ) {
-          const target = mentions.users.first()
-          if (target) {
-            const targetMember = message.guild.members.cache.get(target.id)
-            targetMember.kick()
-            message.channel.send(`${tag} That user has kicked`)
-          } else {
-            message.channel.send(`${tag} Please specify someone to kick.`)
-          }
-        } else {
-          message.channel.send(
-            `${tag} You do not have permission to use this command.`
-          )
-        }
-    })
 });
 
 
