512dbe8ab38178f51d678e63422011a084192e01
MLStan <veldhuizenstan@gmail.com>
2021-04-27 15:12:58 +0200

    reaction roles update

---
+++ commands/reactionrole.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'reactionrole',
+    description: 'Command made by Wascoi',
+    async execute(message, args, Discord, client) {
+        const channel = '835826676273381396';
+        const verifyrole = message.guild.roles.cache.find(role => role.name === "Verified");
+
+        const verifyrole = '✅';
+
+        let embed = new Discord.MessageEmbed()
+        .setcolor(0x2c2f33)
+        .setThumbnail('https://cdn.discordapp.com/attachments/697085154757312603/835573595421540433/Wascoi_logo_1-removebg-preview.png')
+        .setTitle('Verify')
+        .setDescription('To see all the channels you need to react with the emoji under this message.\n\n')
+        + `${verifyrole} To verify`
+        .setTimestamp()
+        .setFooter('Wascoi ✔2021', 'https://cdn.discordapp.com/attachments/697085154757312603/835573595421540433/Wascoi_logo_1-removebg-preview.png');
+
+        let messageEmbed = await message.channel.send(embed);
+    }
+
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const prefix = 'w?';
 
@@ -87,6 +87,10 @@ client.on('message', message => {
 
     if(command === 'ban'){
         client.commands.get('ban').execute(message, args, Discord);
+    }
+
+    if(command === 'reactionrole'){
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
     }
 
 
