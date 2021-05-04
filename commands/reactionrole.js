66b31d492ba7fbb7fd71c3c3138743c4d2151231
MLStan <veldhuizenstan@gmail.com>
2021-05-04 12:14:44 +0200

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -6,13 +6,9 @@ module.exports = {
         if(message.member.permissions.has("ADMINISTRATOR")){
 
         const channel = '836665380935172176';
-        const verifyrole = message.guild.roles.cache.find(role => role.name === "Verified");
-        const travelerrole = message.guild.roles.cache.find(role => role.name === "Traveler");
-        const generalrole = message.guild.roles.cache.find(role => role.name === "- - - - - General - - - - -");
+        const verifyrole = message.guild.roles.cache.find(role => role.name === "WCI | Economy Class");
 
         const verifyemoji = '✅';
-        const traveleremoji = '🛅';
-        const generalemoji = '🈴';
 
         let embed = new Discord.MessageEmbed()
         .setColor('0x2c2f33')
@@ -20,8 +16,8 @@ module.exports = {
         .setTitle('Roles')
         .setTimestamp()
         .setFooter('Wascoi ✔2021', 'https://cdn.discordapp.com/attachments/697085154757312603/835573595421540433/Wascoi_logo_1-removebg-preview.png')
-        .addField('Info', 'To get the all the roles you will need to react with the emoji\'s under this message.\n\n'
-        + `${verifyemoji} To verify - ${traveleremoji} to get trevaler role ${generalemoji} to get general role`);
+        .addField('Info', 'To get the verify role you will need to react with the emoji under this message.\n\n'
+        + `${verifyemoji} To get WCI | Economy Class.`);
 
         let messageEmbed = await message.channel.send(embed);
         messageEmbed.react(verifyemoji);
@@ -36,7 +32,7 @@ module.exports = {
 
             if (reaction.message.channel.id == channel) {
                 if (reaction.emoji.name === verifyemoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(verifyrole, travelerrole, generalrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(verifyrole);
                 }
             } else {
                 return;
